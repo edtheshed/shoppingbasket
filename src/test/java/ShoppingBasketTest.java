5d5fcf50d971c45dad19778fff8aeb7634155fa6
edtheshed <edward.rixon@codurance.com>
2019-11-13 12:17:21 +0000

    anotherrr test

--- src/test/java/ShoppingBasketTest.java
+++ src/test/java/ShoppingBasketTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ShoppingBasketTest {
@@ -11,5 +12,10 @@ public class ShoppingBasketTest {
     @Test
     void second_test() {
         assertTrue(true);
+    }
+
+    @Test
+    void third_test() {
+        assertFalse(false);
     }
 }
